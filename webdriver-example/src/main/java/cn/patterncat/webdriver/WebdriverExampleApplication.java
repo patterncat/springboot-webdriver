73cd0ac3def6e6db404306210cd33e4fca09667c
patterncat <patterncat@gmail.com>
2017-11-04 14:38:43 +0800

    v0.0.4

--- webdriver-example/src/main/java/cn/patterncat/webdriver/WebdriverExampleApplication.java
+++ webdriver-example/src/main/java/cn/patterncat/webdriver/WebdriverExampleApplication.java
@@ -3,9 +3,12 @@ package cn.patterncat.webdriver;
 import cn.patterncat.webdriver.component.DriverProcessor;
 import cn.patterncat.webdriver.component.WebDriverTemplate;
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +17,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"cn.patterncat.webdriver"})
@@ -28,10 +32,17 @@ public class WebdriverExampleApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... strings) throws Exception {
-		webDriverTemplate.execute(10000, new DriverProcessor<Void>() {
+		webDriverTemplate.execute(15000, new DriverProcessor<Void>() {
 			@Override
 			public Void execute(PhantomJSDriver driver) {
 				driver.get("https://www.baidu.com/");
+				try{
+					WebDriverWait webDriverWait = new WebDriverWait(driver, TimeUnit.MILLISECONDS.toSeconds(15000),500); //默认500ms轮询一次
+					webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("demo_wait_class")));
+				}catch (Exception e){
+					//when timeout and element not found
+//					LOGGER.error("web driver wait element timeout",e);
+				}
 				File scrFile = driver.getScreenshotAs(OutputType.FILE);
 				try {
 					FileUtils.copyFile(scrFile, new File("screenshot.jpg"));
