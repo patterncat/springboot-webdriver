bee2d6d8799b3a641fddb6e21a63740c2a8a1300
patterncat <patterncat@gmail.com>
2017-11-04 22:17:54 +0800

    fix

---
+++ springboot-webdriver-starter/src/main/java/cn/patterncat/webdriver/component/DriverLoader.java
@@ -0,0 +1,59 @@
+package cn.patterncat.webdriver.component;
+
+import net.anthavio.phanbedder.Phanbedder;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.util.logging.Level;
+
+/**
+ * Created by patterncat on 2017-11-04.
+ */
+public class DriverLoader {
+
+    private static final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11";
+
+    public static DesiredCapabilities unpackDriverIfNeeded(DriverType driverType){
+        DesiredCapabilities dcaps = new DesiredCapabilities();
+
+        //根据操作系统类型解压embed driver到指定路径,并配置driver路径
+        if(DriverType.CHROME == driverType){
+//            dcaps.setPlatform(Platform.ANDROID);
+            return dcaps;
+        }
+
+        if(DriverType.FIREFOX == driverType){
+
+            return dcaps;
+        }
+
+        //default is phantomjs
+        File phantomjs = Phanbedder.unpack();
+        dcaps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, phantomjs.getAbsolutePath());
+        dcaps.setCapability(PhantomJSDriverService.PHANTOMJS_PAGE_SETTINGS_PREFIX + "userAgent",USER_AGENT);
+        return dcaps;
+    }
+
+    public static WebDriver newInstance(DriverType driverType,DesiredCapabilities dcaps){
+        WebDriver driver = null;
+        if(DriverType.CHROME == driverType){
+            driver = new ChromeDriver();
+            return driver;
+        }
+
+        if(DriverType.FIREFOX == driverType){
+            driver = new FirefoxDriver();
+            return driver;
+        }
+
+        driver = new PhantomJSDriver(dcaps);
+        ((PhantomJSDriver)driver).setLogLevel(Level.INFO);
+        return driver;
+    }
+}
---
+++ springboot-webdriver-starter/src/main/java/cn/patterncat/webdriver/component/DriverType.java
@@ -0,0 +1,8 @@
+package cn.patterncat.webdriver.component;
+
+/**
+ * Created by patterncat on 2017-11-04.
+ */
+public enum DriverType {
+    CHROME,FIREFOX,PHANTOM_JS
+}
